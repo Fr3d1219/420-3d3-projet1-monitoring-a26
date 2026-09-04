34526141b608044115a734c2815b36c08b699e13
Francis Ciza <e2570951@cmaisonneuve.qc.ca>
2026-09-04 12:27:25 -0400

    Ajout Metriquesystem

---
+++ models/metrics.py
@@ -0,0 +1,24 @@
+import psutil
+from models.subject import Sujet
+
+class MetriquesSysteme(Sujet):
+
+    def __init__(self):
+        super().__init__() # Permet d'initialiser la liste des observateurs
+        self._cpu = None
+        self._ram = None
+        self._disque = None
+
+    def actualiser_metriques(self) -> None:
+        self._cpu = psutil.cpu_percent(interval=None)
+        self._ram = psutil.virtual_memory().percent
+        self._disque = psutil.disk_usage('/').percent
+        # À compléter: Appeler notifier()
+        self.notifier()
+
+    def get_donnees(self) -> dict:
+        # À compléter : Retourner un dictionnaire avec cpu, ram, disque
+        donnees = {
+            "cpu" : self._cpu,
+            "ram" : self._ram,
+            "stockage" : self._disque}
